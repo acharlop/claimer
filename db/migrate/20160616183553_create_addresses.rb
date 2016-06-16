230d2f9d91bca2f7c4acc59eb9e9de44394fb3fa
Avi Charlop <avicharlop@gmail.com>
2016-06-16 15:03:58 -0400

    db creation and address migration

--- db/migrate/20160616183553_create_addresses.rb
+++ db/migrate/20160616183553_create_addresses.rb
@@ -2,7 +2,7 @@ class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
       t.string :street,   null: false
-      t.string :apartment,null: true, default: null
+      t.string :apartment,null: true
       t.integer :zip,     null: false
       t.string :city,     null: false
       t.string :state,    null: false, limit: 2, default: "FL"
