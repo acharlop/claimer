a24eb67a3bf4ece3d0518b75dbf1cd12a74be2c8
Avi Charlop <avicharlop@gmail.com>
2016-06-16 22:57:14 -0400

    address polymorphic association

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
-	# validation using google maps?
+	belongs_to :full_address, polymorphic: true
+	
 	validates :street, :zip, :city, :state, presence: true
 	validates :zip, numericality: { greater_than_or_equal_to: 10000, less_than: 99950 }
 
