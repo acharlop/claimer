25b15a8a33c614c459ad1facaac21fbc3bce1a5a
Avi Charlop <avicharlop@gmail.com>
2016-06-17 22:17:21 -0400

    email downcase

--- app/models/email.rb
+++ app/models/email.rb
@@ -3,4 +3,12 @@ class Email < ApplicationRecord
 
 	validates_format_of :email,:with => Devise::email_regexp
 	validates :description, presence: true
+
+	before_save :case_cleanup
+
+	private
+
+	def case_cleanup
+		self.email.downcase!
+	end
 end
